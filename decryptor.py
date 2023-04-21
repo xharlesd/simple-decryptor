cb46b0e4843bfe65b22d701320d005cd49ec4f7a
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-21 15:57:15 +0800

    try-again function

--- decryptor.py
+++ decryptor.py
@@ -9,8 +9,28 @@ from colorama import Fore, Style
 import time
 
 # Create a try-again function to use the program again if the user wants to 
-    # the program will run again if the user inputted YES
-    # the program will terminate if the user inputted NO
+def try_again():
+    retry = None
+    while retry is None:
+        time.sleep(1)
+        again = input(Fore.MAGENTA + "\n\t\t\tDo you want to try again? (YES/NO)  ")
+        # the program will run again if the user inputted YES
+        if again == "Y" or again == "Yes" or again == "YES" or again == "yes":
+            retry = str(again)
+            continue
+        # the program will terminate if the user inputted NO
+        if again == "N" or again == "No" or again == "NO" or again == "no":
+            time.sleep(0.5)
+            print(Fore.GREEN + "\t\t\t[Program will be terminated..............]")
+            time.sleep(1.5)
+            print("\n")
+            print("\033[0;34m" + "\033[1m-" * 140 + '\033[0m')
+            print(" ")
+            lab = pyfiglet.figlet_format("   THANK YOU !!   ", font = "banner3",  width = 140, justify = "center")
+            print(Style.BRIGHT + Fore.CYAN + lab)
+            exit()
+        else:
+            print(Fore.RED + "\t\t\t[ERROR] Please choose either YES or NO only.")
 
 
 # Use pyfiglet formatting to "Lab. Exercise No. 1"
