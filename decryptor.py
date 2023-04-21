99ecbfcbe075fe3899ac3c1d9b042f65c0a6dc34
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-21 15:52:39 +0800

    pseudocodee

---
+++ decryptor.py
@@ -0,0 +1,22 @@
+# Bernido, Charles David P. | BSCPE 1-5
+# Object Oriented Programming | Lab Exercise 1 - Problem 2
+
+
+# import modules
+
+# Create a try-again function to use the program again if the user wants to 
+    # the program will run again if the user inputted YES
+    # the program will terminate if the user inputted NO
+
+
+# Use pyfiglet formatting to "Lab. Exercise No. 1"
+# format introductory message
+
+# insert time delay
+
+# while loop
+    # ask user to input the encrypted text
+    # replace each character ( '* to a', '& to e', '# to i', '+ to o', '! to u' )
+    # display decrypted text
+
+    # call try-again function
